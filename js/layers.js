858bc16e82ffd7cdcb5c7ba00a65c6664c31993b
Yahkub7 <Yahkub@gmail.com>
2022-12-28 00:20:59 -0800

    0.0.0.0.6.9
    version 0.0.0.0.6.9

--- js/layers.js
+++ js/layers.js
@@ -5,60 +5,200 @@ addLayer("C", {
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+        best: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1.5), // Can be a function that takes requirement increases into account
     resource: "Conviction", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "Faith", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
+    canBuyMax: true,
     gainMult() {
         let mult = new Decimal(1)
-        if (hasUpgrade('C', 13)) mult = mult.times(upgradeEffect('C', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        let exp = new Decimal(1)
+        return exp
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "c", description: "c: Reset for conviction", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
+    update(diff) {
+        player.points = player.points.minus(player.points.mul(getDegen()).mul(diff))
+    },
     upgrades: {
         11: {
             title: "Belief",
-            description: "Double your faith gain.",
-            cost: new Decimal(1),
+            description: "Double your Faith gain.",
+            cost: new Decimal(2),
+            effectDisplay() {
+                return "2x"
+            },
         },
         12: {
             title: "Prayer",
-            description: "Increase faith gain based on current conviction",
+            description: "Square your Faith gain.",
             cost: new Decimal(2),
-            effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+            effectDisplay() {
+                return "x^2"
             },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
         13: {
-            title: "Belief3",
-            description: "Double your faith gain.",
-            cost: new Decimal(4),
+            title: "Zeal",
+            description: "Multiply Faith gain by 1+log10(Best Conviction)",
+            cost: new Decimal(2),
             effect() {
-                return player.points.add(1).pow(0.15)
+                let Eff  = new Decimal(1)
+                Eff = Eff.add(player.C.best)
+                Eff = Eff.log10()
+                Eff = Eff.add(1)
+                return Eff
             },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+            effectDisplay() {
+                return format(upgradeEffect("C",13)) + "x"
+            },
         },
-        14: {
-            title: "Belief4",
-            description: "Double your faith gain.",
-            cost: new Decimal(6),
+        21: {
+            title: "Flagellation",
+            description: "reduce your Faith lost to inaction",
+            cost: new Decimal(25),
+            effectDisplay() {
+                return "-5%"
+            }
         },
-        15: {
-            title: "Belief5",
-            description: "Double your faith gain.",
-            cost: new Decimal(8),
+    },
+}),
+addLayer("A", {
+    name: "Achievements", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+        points: new Decimal(0),
+    }},
+    tooltip() {
+      return "Achievements"
+    },
+    color: "#FFFF00",
+    nodeStyle() {return {
+        "background": "radial-gradient(#FFFF00, #d5ad83)" ,
+    }},
+    requires: new Decimal(0), // Can be a function that takes requirement increases into account
+    resource: "Achievement Points",
+    resourceSingular: "Achievement Point", 
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    row: "side", // Row the layer is in on the tree (0 is the first row)
+    layerShown() { return true },
+    effect(){
+        let acheff = player[this.layer].points.add(1)
+        acheff = acheff.log10()
+        acheff = acheff.div(10)
+        return acheff
+    },
+    effectDescription() {
+        let dis = "<br> which boosts base Faith gain by " + format(tmp.A.effect)
+        return dis
+    },
+    achievements: {
+        rows: 23,
+        cols: 6,
+        11: {
+            name: "That.. that's a nerf..",
+            doneTooltip: "Add 0.1 to base Faith gain",
+            unlocked() {if (hasAchievement("A",11)) return true
+            },
+            done() {
+                if (hasUpgrade("C",12))
+                return getPointGen()<1
+            },
+            onComplete() {
+                addPoints("A",1)
+            }
         },
+        21: {
+            name: "Believe",
+            tooltip: "Get your first point of Conviction",
+            done() {
+                return player.C.points.gte(1)
+            },
+            onComplete() {
+                addPoints("A",1)
+            },
+        },
+        22: {
+            name: "Believe More",
+            tooltip: "Get your first point of Conviction",
+            done() {
+                return player.C.points.gte(100)
+            },
+            onComplete() {
+                addPoints("A",1)
+            },
+        },
+        23: {
+            name: "Believe Moar",
+            tooltip: "Get your first point of Conviction",
+            done() {
+                return player.C.points.gte(1e5)
+            },
+            onComplete() {
+                addPoints("A",1)
+            },
+        },
+        24: {
+            name: "Believe Most",
+            tooltip: "Get your first point of Conviction",
+            done() {
+                return player.C.points.gte(1e7)
+            },
+            onComplete() {
+                addPoints("A",1)
+            },
+        },
+        25: {
+            name: "Believe Mostester",
+            tooltip: "Get your first point of Conviction",
+            done() {
+                return player.C.points.gte(1e9)
+            },
+            onComplete() {
+                addPoints("A",1)
+            },
+        },
+    }
+}),
+addLayer("Ch", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: true,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+    color: "#4BDC13",                       // The color for this layer, which affects many elements.
+    resource: "Gold",                       // The name of this layer's main prestige resource.
+    row: 1,                                 // The row this layer is on (0 is the first row).
+    position: 0,
+    baseResource: "Conviction",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.C.points },  // A function to return the current amount of baseResource.
+    requires: new Decimal(1),               // The amount of the base needed to  gain 1 of the prestige currency. 
+                                            // Also the amount required to unlock the layer.
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+    hotkeys: [
+        {key: "h", description: "h: Reset for church gold", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+    upgrades: {
+        // Look in the upgrades docs to see what goes here!
     },
 })
--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (9), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
@@ -20,14 +20,14 @@ let VERSION = {
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
 		- Added things.<br>
-		- Broke things.
-		- Tried to make one layer work.
-		- I don't know any coding so this is just a logic puzzle
-		- Do you know how long it took to find out how to raise gain to a power?
-		- It was too long. Way too long. I google'd it, I tried everything I could think of.
-		- Then, then after a week, I had the bright idea to look at someone else's mod.
-		- Why didn't I do that earlier? Cause I'm stupid.
-		- This logic puzzle is going to be hard.`
+		- Broke things.<br>
+		- Tried to make one layer work.<br>
+		- I don't know any coding so this is just a logic puzzle.<br>
+		- Do you know how long it took to find out how to raise gain to a power?<br>
+		- It was too long. Way too long. I google'd it, I tried everything I could think of.<br>
+		- Then, then after a week, I had the bright idea to look at someone else's mod.<br>
+		- Why didn't I do that earlier? Cause I'm stupid.<br>
+		- This logic puzzle is going to be hard.<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -35,24 +35,43 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){
+function getStartPoints() {
     return new Decimal(modInfo.initialStartPoints)
 }
 
 // Determines if it should show points/sec
-function canGenPoints(){
-	return true
+function canGenPoints() {
+	return false
 }
 
 // Calculate points/sec!
+function getPointBase() {
+	let base = new Decimal(0.5)
+	if (hasAchievement("A",11)) base = base.add(0.1)
+	base = base.add(tmp.A.effect)
+	return base
+}
+function getPointMult() {
+	let mult = new Decimal(1)
+	if (hasUpgrade("C",11)) mult = mult.add(1)
+	if (hasUpgrade("C",13)) mult = mult.mul(upgradeEffect("C",13))
+	return mult
+}
+function getPointExp() {
+	let exp = new Decimal(1)
+	if (hasUpgrade("C",12)) exp = exp.add(1)
+	return exp
+}
 function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
-		
-	let gain = new Decimal(1)
-	if (hasUpgrade('C', 11)) gain = gain.pow(2)
-	if (hasUpgrade('C', 12)) gain = gain.times(upgradeEffect('C', 12))
+	let gain = getPointBase()
+	gain = gain.mul(getPointMult())
+	gain = gain.pow(getPointExp())
 	return gain
+}
+function getDegen() {
+	let degen = new Decimal(0.10)
+	if (hasUpgrade("C",21)) degen = degen.minus(0.05)
+	return degen
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
@@ -61,6 +80,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
+	function() {
+		let dis = getPointGen()
+		dis = dis.minus(player.points.mul(getDegen()))
+		dis = "("+format(dis)+") Net F/s<br> You lose " + getDegen().mul(100) + "% of your total Faith per second"
+		return "(" + format(getPointGen()) + ")" + " Gross F/s<br>"+dis
+	}
 ]
 
 // Determines when the game "ends"
